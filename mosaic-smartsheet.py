b9c1702f8cc8678aa7dbe732d35306879b4b60ef
jmusarra <github@mightymu.net>
2023-10-24 12:11:59 -0700

    working on get_sheet

--- mosaic-smartsheet.py
+++ mosaic-smartsheet.py
@@ -39,8 +39,14 @@ def check_for_internet(hostname):
         pass
     return False
 
-def get_sheet(sheet):
-    print(f'Sheet to get: {sheet}')
+def get_sheet(sheet_name):
+    ss_client = smartsheet.Smartsheet()
+    ss_client.errors_as_exceptions(True)
+    print(f'Sheet to get: {sheet_name}')
+    search_results = ss_client.Search.search(sheet_name).results
+    sheet_id = next(result.object_id for result in search_results if result.object_type == 'sheet')
+    ss_client.Sheets.get_sheet(sheet_id)
+    print(f'Sheet ID is {sheet_id}')
 
 def make_csv(dmx_line, zone_number):
     pass
@@ -50,11 +56,11 @@ def extract_zones():
 
 if __name__ == '__main__':
     arguments = parser.parse_args()
-    sheet = arguments.sheet_name
-    print(sheet)
+    sheet_name = arguments.sheet_name
+    print(sheet_name)
     type(arguments)
     if check_for_internet(REMOTE_SERVER):
         print("looks like we have internet. Let's continue.")
     else:
         sys.exit('No internet connection found. Exiting. (try again later?)')
-    get_sheet(sheet)
+    get_sheet(sheet_name)
