0dd614c28dda524a5c7e8eeaeaa2999784c29466
jmusarra <github@mightymu.net>
2023-10-27 11:11:48 -0700

    add TODOs to fixture_types

--- fixture_types.py
+++ fixture_types.py
@@ -1,1 +1,3 @@
 print('TODO: we need a table of fixture types (LED RGBW 16bit) to "Model IDs" (12)')
+# Needs to be able to resolve the "Fixture Type" column values into a more generic
+#  fixture type that Mosaic can use. This will take some time.
